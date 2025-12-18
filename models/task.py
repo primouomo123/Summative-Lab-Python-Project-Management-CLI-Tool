2c2c40db7391ed7030448309de7ff7457c577c62
Luis Rios Alvarado <luismanuelrios123@gmail.com>
2025-12-18 16:58:45 -0500

    Pydantic implemented

--- models/task.py
+++ models/task.py
@@ -1,39 +1,17 @@
-from models.base import BaseModel
-from models.project import Project
+from models.base import Entity
 
-class Task(BaseModel):
-    all_by_id = {}
+class Task(Entity):
+    title: str
+    project_id: str  # ID of the Project this task belongs to
+    status: str = "pending"  # default status
 
-    def __init__(self, title, project_id, status="pending", id=None):
-        super().__init__(id)
-        self.title = title
-        self.project_id = project_id  # store Project ID for JSON
-        self.status = status
-        Task.all_by_id[self.id] = self
-
-    @property
-    def project(self):
-        return Project.all_by_id.get(self.project_id)
-
-    @property
-    def user(self):
-        """Return the user assigned to this task through its project"""
-        proj = self.project
-        return proj.user if proj else None
-
-    def to_dict(self):
-        return {
-            "id": self.id,
-            "title": self.title,
-            "status": self.status,
-            "project_id": self.project_id
-        }
+    # class-level list to store all instances
+    all = []
+    def __init__(self, **data):
+        super().__init__(**data)
+        Task.all.append(self)
 
     @classmethod
-    def from_dict(cls, data):
-        return cls(
-            title=data["title"],
-            project_id=data["project_id"],
-            status=data.get("status", "pending"),
-            id=data["id"]
-        )
+    def from_dict(cls, data: dict):
+        """Load from dict"""
+        return cls.model_validate(data)
